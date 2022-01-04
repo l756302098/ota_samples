76c424deedb7c420c609e325cfa38d51b5c9705a
li <liyanlong@indemind.cn>
2022-01-04 11:12:54 +0800

    update ota

--- scripts/ota.sh
+++ scripts/ota.sh
@@ -2,7 +2,7 @@
 BASE_NAME=$(basename $0)
 #EXE_NAME=$(basename $1)
 FTP_SERVER=192.168.50.191
-EXE_NAME=INDEMINDAPP_ICE_EVT3.1_1.1.3.0.deb
+EXE_NAME=cn.ota_samples-1.0.0-Linux.deb
 echo $EXE_NAME
 echo "$BASE_NAME start. $(date +%Y/%m/%d-%H:%M:%S)" >> /tmp/$BASE_NAME.log
 #cd workspaces
@@ -10,7 +10,7 @@ cd /home/li/workspaces
 #remove old deb
 rm -rf $EXE_NAME
 #ftp download
-wget --user=guest --password='guest' ftp://$FTP_SERVER/release/INDEMINDAPP/$EXE_NAME
+wget --user=guest --password='guest' ftp://$FTP_SERVER/test_app/$EXE_NAME
 if [[ "$?" -eq 0 ]] ; then
     echo "wget successful." >> /tmp/$BASE_NAME.log
     echo -e  "\033[32m wget succeful!  \033[0m"
@@ -37,14 +37,24 @@ else
     echo -e "\033[31m install deb failed! \033[0m"
     exit 0;
 fi
+#add exe permissions
+sudo chmod +x /home/li/workspaces/scripts/*.sh
+if [[ "$?" -eq 0 ]] ; then
+    echo "chmod +x scripts successful." >> /tmp/$BASE_NAME.log
+    echo -e "\033[32m chmod +x scripts succeful!  \033[0m"
+else
+    echo "chmod +x scripts failed. " >> /tmp/$BASE_NAME.log
+    echo -e "\033[31m chmod +x scripts failed! \033[0m"
+    exit 0;
+fi
 #set init.d
 cd /home/li/workspaces/scripts
 sudo ./install.sh
 if [[ "$?" -eq 0 ]] ; then
-    echo "install successful." >> /tmp/$BASE_NAME.log
-    echo -e "\033[32m install succeful!  \033[0m"
+    echo "set init.d successful." >> /tmp/$BASE_NAME.log
+    echo -e "\033[32m set init.d succeful!  \033[0m"
 else
-    echo "install failed. " >> /tmp/$BASE_NAME.log
-    echo -e "\033[31m install failed! \033[0m"
+    echo "set init.d failed. " >> /tmp/$BASE_NAME.log
+    echo -e "\033[31m set init.d failed! \033[0m"
     exit 0;
 fi
